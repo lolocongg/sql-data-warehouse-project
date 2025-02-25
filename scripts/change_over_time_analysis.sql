f76f67478753fcd405577d2579c43b79587f9913
lolocongg <111275504+lolocongg@users.noreply.github.com>
2025-02-25 10:55:31 +0700

    Update change_over_time_analysis.sql

--- scripts/change_over_time_analysis.sql
+++ scripts/change_over_time_analysis.sql
@@ -26,7 +26,7 @@ WHERE order_date IS NOT NULL
 GROUP BY YEAR(order_date), MONTH(order_date)
 ORDER BY YEAR(order_date), MONTH(order_date);
 
--- DATETRUNC()
+-- DATETRUNC() (order_date)
 SELECT
     DATETRUNC(month, order_date) AS order_date,
     SUM(sales_amount) AS total_sales,
@@ -37,7 +37,7 @@ WHERE order_date IS NOT NULL
 GROUP BY DATETRUNC(month, order_date)
 ORDER BY DATETRUNC(month, order_date);
 
--- FORMAT()
+-- FORMAT() (order_date)
 SELECT
     FORMAT(order_date, 'yyyy-MMM') AS order_date,
     SUM(sales_amount) AS total_sales,
